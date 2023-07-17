08942455e5f792cc4b93da9460c45d71e28d5205
sSerpeD <80467475+sSerpeD@users.noreply.github.com>
2023-07-18 00:38:08 +0700

    update : yeeyeeass ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import GMap from './Map';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import GMap from "./Map";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GMap />
+    <App />
   </React.StrictMode>
 );
 
